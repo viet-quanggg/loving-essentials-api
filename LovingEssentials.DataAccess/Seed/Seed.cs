1e598f711e8cd804a6a0322f18c2b29218ed3fa8
nguyenphuongkiet <KietNPSE172931@fpt.edu.vn>
2024-06-18 10:50:50 +0700

    Seed User

---
+++ LovingEssentials.DataAccess/Seed/Seed.cs
@@ -0,0 +1,29 @@
+﻿using LovingEssentials.BusinessObject;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LovingEssentials.DataAccess.Seed
+{
+    public class Seed
+    {
+        public static async Task SeedUser(DataContext _context)
+        {
+            if (await _context.Users.AnyAsync()) return;
+
+            var userData = await File.ReadAllTextAsync("../LovingEssentials.DataAccess/Seed/UserSeed.json");
+            var jsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var users = JsonSerializer.Deserialize<List<User>>(userData, jsonOptions);
+
+            foreach (var user in users)
+            {
+                await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
