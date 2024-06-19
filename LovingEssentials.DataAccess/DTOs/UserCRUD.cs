1349b2afd7b765b1c057bd5bb05c5f06b957dc7c
caothang7a7 <caothang7a7@gmail.com>
2024-06-19 16:10:54 +0700

    fix bug

--- LovingEssentials.DataAccess/DTOs/UserCRUD.cs
+++ LovingEssentials.DataAccess/DTOs/UserCRUD.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿using LovingEssentials.BusinessObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LovingEssentials.DataAccess.DTOs
 {
-    public enum Role
+    /*public enum Role
     {
+        [EnumMember(Value = "Client")]
         Client = 1,
+        [EnumMember(Value = "Shipper")]
         Shipper = 2,
+        [EnumMember(Value = "Admin")]
         Admin = 3
-    }
+    }*/
     public class UserCRUD
     {
         public int Id { get; set; }
